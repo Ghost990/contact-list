f009c4e53b1e461e1e24bd4d404a7b79bc9e46e6
Zsolt Nagy <zsoltnagy@Zsolts-MacBook-Pro.local>
2018-01-04 10:31:27 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactPanelComponent } from './contact-panel/contact-panel.component';
+import { EditContactComponent } from './contact-panel/edit-contact/edit-contact.component';
+import { NewContactComponent } from './contact-panel/new-contact/new-contact.component';
+import { ContactSearchComponent } from './contact-list/contact-search/contact-search.component';
+import { ContactListItemComponent } from './contact-list/contact-list-item/contact-list-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactPanelComponent,
+    EditContactComponent,
+    NewContactComponent,
+    ContactSearchComponent,
+    ContactListItemComponent
   ],
   imports: [
     BrowserModule
