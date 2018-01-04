1bf8ae7b67c011b1cef5670b75e5cb0fc8b4aaf0
Zsolt Nagy <zsoltnagy@Zsolts-MacBook-Pro.local>
2018-01-04 18:57:26 +0100

    added profile pictures for the contacts, added comments to *.ts and *.html files

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,3 +1,10 @@
+/**
+ * The contact-list.component.ts file is responsible for handling the JSON request from the
+ * folder, subscibing for it and use use an array where we can store all of the objects.
+ * After the objects are in place we can use in the template for listing all the contacts
+ * from the API, in this case from the /assets/generated.json file.
+ **/
+
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
@@ -15,16 +22,24 @@ export class ContactListComponent implements OnInit {
 
 
   constructor(private http: Http) {
+
+    /** Getting the JSON and sorting it alphabetically with the name keys **/
+
     http.get(this.url).subscribe(response => {
       this.contacts = response.json();
       this.contacts.sort(function(a, b) {
         return a.name > b.name;
       });
-      this.contacts.sort();
+
+      /** Iterating through the contacts array and saving the first letter in another one **/
 
       for (let contact of this.contacts) {
         this.firstLetter.push(contact.name.charAt(0));
       }
+
+      /** Removing duplicates from the array for every same starting letter
+       *  will be grouped under a single letter.
+       **/
 
       this.removeDuplicates(this.firstLetter);
       console.log(this.uniqueArray);
