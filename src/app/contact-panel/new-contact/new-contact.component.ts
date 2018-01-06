6e05b4f11318ac4708d3877f0f31caebcff4c83d
Zsolt Nagy <zsoltnagy@Zsolts-MacBook-Pro.local>
2018-01-06 18:30:38 +0100

    working creating contact with default profile picture

--- src/app/contact-panel/new-contact/new-contact.component.ts
+++ src/app/contact-panel/new-contact/new-contact.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, EventEmitter } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatButton, MatFormFieldModule, MatSelect } from '@angular/material';
 import {NgForm, NgModel, FormGroup} from '@angular/forms';
+import { UploadOutput, UploadInput, UploadFile, humanizeBytes, UploaderOptions } from 'ngx-uploader';
 
 @Component({
 
@@ -11,28 +12,14 @@ export class NewContactComponent {
   contacts = [];
 
 
-
   constructor(public thisDialogRef: MatDialogRef<NewContactComponent>, @Inject(MAT_DIALOG_DATA) public data: string) {
     this.contacts = JSON.parse(localStorage['contacts']);
   }
 
-  onCloseConfirm() {
-    this.thisDialogRef.close('Confirm');
-  }
-
-  onCloseCancel() {
-    this.thisDialogRef.close('Cancel');
-  }
-
-  addNewContact() {
-    //this.contacts.push({_id: 20, name: 'Amenadiel', picture: 'assets/profiles/people-q-c-64-64-7.jpg', email: 'none', phone: 'none', isFavorite: true, company: 'google'});
-    //localStorage.setItem('contacts', JSON.stringify(this.contacts));
-    //console.log(form.value);
-  }
-
   onSubmit(name: string, phone: number, email: string, group: string) {
-    this.contacts.push({_id: 20, name: name, picture: 'assets/profiles/people-q-c-64-64-7.jpg', email: email, phone: phone, isFavorite: true, company: 'google'});
+    this.contacts.push({_id: 20, name: name, picture: 'assets/profiles/default.png', email: email, phone: phone, isFavorite: true, company: 'google'});
     localStorage.setItem('contacts', JSON.stringify(this.contacts));
   }
+
 
 }
